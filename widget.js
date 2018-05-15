6735a4462b5c3703608bd072fce81f5e7647c8a5
PJ <peter@kustomer.com>
2018-05-15 13:45:39 -0400

    fixed code

--- widget.js
+++ widget.js
@@ -5,3 +5,11 @@ $.urlParam = function(name){
 	}
 	return null;
 }
+
+jQuery.extend({
+
+  getQueryParameters : function(str) {
+	  return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
+  }
+
+});
